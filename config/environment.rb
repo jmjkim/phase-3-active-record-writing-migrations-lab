273f57f196d11148406f948591d71ab7650b3de5
Jongmin Jack Kim <jmjkim00@icloud.com>
2022-07-18 21:39:42 -0700

    Completed assignment

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,8 @@ require 'bundler/setup'
 Bundler.require(:default, ENV["RACK_ENV"])
 
 require_relative "../app/models/student"
+
+ActiveRecord::Base.establish_connection(
+    adapter: "sqlite3",
+    database: "db/development.sqlite3"
+)
